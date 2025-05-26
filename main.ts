c9447e79aa70d598f4bafa03aaa2172a3123aafd
Filip <filip@funbutler.com>
2025-05-26 16:15:22 +0200

    .

--- main.ts
+++ main.ts
@@ -7,7 +7,11 @@ const app = express();
 
 // Serve static files from the "assets" directory
 //app.use("/public",express.static(path.join(Deno.cwd(), "public"), { etag: false}));
-app.use("/less-css", expressLess(path.join(Deno.cwd(), "public" , "less"), { compress: true }));
+app.use("/less-css", (req, res, next) => {
+    const oneWeek = 7 * 24 * 60 * 60;
+    res.setHeader("Cache-Control", `public, max-age=${oneWeek}`);
+    next();
+},expressLess(path.join(Deno.cwd(), "public", "less"), { compress: true }));
 
 // Set the view engine to EJS
 app.get("/", (req: any, res: any) => {
